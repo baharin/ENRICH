4350d87245200e5ff761668f462889796f4f97e5
Baharin A. Jodat <25382501+baharin@users.noreply.github.com>
2022-09-29 16:04:41 -0400

    Update RQ2 - Statistical Test.py

--- Evaluation/RQ2/RQ2 - Statistical Test.py
+++ Evaluation/RQ2/RQ2 - Statistical Test.py
@@ -6,8 +6,14 @@
 from scipy.stats import mannwhitneyu
 import pandas as pd
 
-sohosim = pd.read_excel('sohosim.xlsx')
-sohohw = pd.read_excel('sohohw.xlsx')
+data = pd.read_excel('SOHOSim VS SOHOHW.xlsx', header = 0)
+
+sohosim = []
+sohohw = []
+
+for i in range(len(data.index)):
+  sohosim.append(data.loc[i, 'SOHOSIM Robustness Measure'])
+  sohohw.append(data.loc[i, 'SOHOHW Robustness Measure'])
 
 
 U1, pvalue  = mannwhitneyu(sohosim,sohohw)
